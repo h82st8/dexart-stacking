68b3dd0f6b701956671b9c8e245bc7ef99b6f683
rusaliyev <rus.aliev.117@mail.ru>
2023-08-10 15:55:05 +0300

    chore: open sales for aton users

---
+++ pages/aton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <CommonLoader :size="80" />
+  </div>
+</template>
+
+<script>
+import Cookies from 'js-cookie';
+import { CommonLoader } from '@/components/CommonLoader.vue';
+
+export default {
+  name: 'AdminDexfarmPage',
+
+  components: {
+    CommonLoader,
+  },
+
+  mounted() {
+    Cookies.set('atonUser', 527, { expires: new Date(process.env.NUXT_ENV_DEADLINE) });
+    window.location.assign('/');
+  },
+};
+</script>
