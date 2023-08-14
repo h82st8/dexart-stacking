0d17dca55af7056692dc3038effc2957b35316f6
rusaliyev <rus.aliev.117@mail.ru>
2023-08-14 16:05:48 +0300

    fix: bug with buy tokens

--- store/index.js
+++ store/index.js
@@ -166,11 +166,16 @@ export const actions = {
       commit('SET_ERROR_MESSAGE', response.message);
 
       if (response.data) {
-        commit('SET_BUY_STATE', {state: FULFILLED, data: response.data.link});
         if (response.data.link.includes('https')) {
           // hach for Google Analutics
-          setTimeout(() => openMerchant(response), 1000)
+          setTimeout(() => {
+            openMerchant(response)
+            commit('SET_BUY_STATE', {state: FULFILLED, data: response.data.link});
+          }, 500)
         }
+        setTimeout(() => {
+          commit('SET_BUY_STATE', {state: FULFILLED, data: response.data.link});
+        }, 1000)
       } else {
         commit('SET_BUY_STATE', {state: REJECTED})
       }
